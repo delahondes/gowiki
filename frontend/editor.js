c8d43a6c5284f1f9900c63d627ae8a9f50074dd0
Raynald de Lahondes <raynald@delahondes.com>
2025-12-29 18:20:36 +0100

    Add new paragraph, line breaks, undo, redo.

--- frontend/editor.js
+++ frontend/editor.js
@@ -3,6 +3,40 @@ import { EditorState } from "prosemirror-state";
 import { EditorView } from "prosemirror-view";
 import { schema } from "prosemirror-schema-basic";
 import { defaultMarkdownParser, defaultMarkdownSerializer } from "prosemirror-markdown";
+import { keymap } from "prosemirror-keymap";
+import { baseKeymap, exitCode, chainCommands } from "prosemirror-commands";
+import { history } from "prosemirror-history";
+import { undo, redo } from "prosemirror-history";
+
+function insertHardBreakCommand(state, dispatch) {
+  const br = state.schema.nodes.hard_break;
+  if (!br) return false;
+
+  if (dispatch) {
+    dispatch(
+      state.tr
+        .replaceSelectionWith(br.create())
+        .scrollIntoView()
+    );
+  }
+  return true;
+}
+
+function hardBreakWithExitCode(state, dispatch, view) {
+  return chainCommands(
+    exitCode,
+    insertHardBreakCommand
+  )(state, dispatch, view);
+}
+
+const customKeymap = {
+  "Shift-Enter": hardBreakWithExitCode,
+  "Mod-Enter": hardBreakWithExitCode,
+  "Ctrl-Enter": hardBreakWithExitCode,
+  "Mod-z": undo,
+  "Mod-y": redo,
+  "Mod-Shift-z": redo,
+};
 
 let view = null;
 
@@ -20,6 +54,11 @@ window.initProseMirrorEditor = function (container, sourceTextarea) {
   const state = EditorState.create({
     doc: defaultMarkdownParser.parse(content),
     schema: schema,
+    plugins: [
+      history(),
+      keymap(customKeymap),
+      keymap(baseKeymap),
+    ],
   });
 
   view = new EditorView(container, {
